745219c91b876f0d127cbcdc7db88f9ccb5e7712
kchatr <kchatr1729@gmail.com>
2020-11-07 14:08:42 -0500

    Added Description & Aliases

--- cmd/delete.go
+++ cmd/delete.go
@@ -16,24 +16,20 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Aliases: []string{"del", "rm"},
+	Short: "Delete a To-Do",
+	Long: `Delete a To-Do from your To-Do list. This cannot be undone.`,
+	Run: deleteRun,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
-	},
+func deleteRun(cmd *cobra.Command, args []string) {
+
 }
 
 func init() {
